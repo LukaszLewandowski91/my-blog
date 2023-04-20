a27fd20fcbb8471cc7b237bb02bac1fcf9e757bd
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-04-20 07:19:57 +0200

    fix bugs in Posts.js

--- src/components/features/Posts/Posts.js
+++ src/components/features/Posts/Posts.js
@@ -9,20 +9,18 @@ const Posts = () => {
     <Container>
       <Row>
         {posts.map((post) => (
-          <Col md={4} sm={12} className="my-1">
+          <Col key={post.id} md={4} sm={12} className="my-1">
             <Card style={{ width: "100%" }}>
               <Card.Body>
                 <Card.Title>{post.title}</Card.Title>
-                <Card.Text>
-                  <p className={styles.p}>
-                    Author: <span className={styles.span}>{post.author}</span>
-                  </p>
-                  <p className={styles.p}>
-                    Published:{" "}
-                    <span className={styles.span}>{post.publishedDate}</span>
-                  </p>
-                  <span>{post.shortDescription}</span>
+                <Card.Text className={styles.p}>
+                  Author: <span className={styles.span}>{post.author}</span>
                 </Card.Text>
+                <Card.Text className={styles.p}>
+                  Published:{" "}
+                  <span className={styles.span}>{post.publishedDate}</span>
+                </Card.Text>
+                <Card.Text>{post.shortDescription}</Card.Text>
                 <Button variant="primary" href={"/post/" + post.id}>
                   Read more
                 </Button>
