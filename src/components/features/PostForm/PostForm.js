359c603d5ab365bf50b271ac2424e6a1dce3cb7d
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-04-26 20:58:44 +0200

    add validation in PostForm.js

--- src/components/features/PostForm/PostForm.js
+++ src/components/features/PostForm/PostForm.js
@@ -5,47 +5,80 @@ import ReactQuill from "react-quill";
 import DatePicker from "react-datepicker";
 import { registerLocale } from "react-datepicker";
 import pl from "date-fns/locale/pl";
+import { useForm } from "react-hook-form";
 
 const PostForm = ({ action, actionText, ...props }) => {
+  const [contentError, setContentError] = useState(false);
+  const [dateError, setDateError] = useState(false);
+  registerLocale("pl", pl);
+
+  const {
+    register,
+    handleSubmit: validate,
+    formState: { errors },
+  } = useForm();
+
   const [title, setTitle] = useState(props.title || "");
   const [author, setAuthor] = useState(props.author || "");
   const [publishedDate, setPublishedDate] = useState(props.publishedDate || "");
   const [shortDescription, setShortDescription] = useState(
     props.shortDescription || ""
   );
-  registerLocale("pl", pl);
+
   const [content, setContent] = useState(props.content || "");
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    action({ title, author, publishedDate, shortDescription, content });
+
+  const handleSubmit = () => {
+    setContentError(!content);
+    setDateError(!publishedDate);
+    if (content && publishedDate) {
+      action({ title, author, publishedDate, shortDescription, content });
+    }
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={validate(handleSubmit)}>
       <Row className="mb-3">
         <Form.Group as={Col}>
           <Form.Label>Title</Form.Label>
           <Form.Control
+            {...register("title", { required: true, minLength: 3 })}
             value={title}
             className="mb-3"
             placeholder="Enter Title"
-            required
             onChange={(e) => setTitle(e.target.value)}
+            type="text"
           />
+          {errors.title && (
+            <small className="d-block form-text text-danger mt-2">
+              Title is too short (min is 3)
+            </small>
+          )}
           <Form.Label>Author</Form.Label>
           <Form.Control
+            {...register("author", { required: true, minLength: 3 })}
             value={author}
             className="mb-3"
             placeholder="Enter Author"
-            required
             onChange={(e) => setAuthor(e.target.value)}
+            type="text"
           />
+          {errors.author && (
+            <small className="d-block form-text text-danger mt-2">
+              Author is too short (min is 3)
+            </small>
+          )}
           <Form.Label>Published</Form.Label>
           <DatePicker
             selected={publishedDate}
             onChange={(date) => setPublishedDate(date)}
             locale="pl"
+            className="mb-3"
           />
+          {dateError && (
+            <small className="d-block form-text text-danger mt-2">
+              This field is required
+            </small>
+          )}
           <Form.Label>Short description</Form.Label>
           <FloatingLabel
             controlId="floatingTextarea"
@@ -53,17 +86,30 @@ const PostForm = ({ action, actionText, ...props }) => {
             className="mb-3"
           >
             <Form.Control
+              {...register("shortDescription", {
+                required: true,
+                minLength: 20,
+              })}
               value={shortDescription}
               as="textarea"
               className={styles.shortDescription}
               placeholder="Leave a comment here"
-              required
               style={{ height: "120px" }}
               onChange={(e) => setShortDescription(e.target.value)}
             />
+            {errors.shortDescription && (
+              <small className="d-block form-text text-danger mt-2">
+                Short description is too short (min is 20)
+              </small>
+            )}
           </FloatingLabel>
           <Form.Label>Main content</Form.Label>
           <ReactQuill theme="snow" value={content} onChange={setContent} />
+          {contentError && (
+            <small className="d-block form-text text-danger mt-2">
+              This field is required
+            </small>
+          )}
         </Form.Group>
       </Row>
       <Button variant="primary" type="submit">
